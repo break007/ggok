614844e3fdfd7e175b4160adf08ecce129e52a41
break007 <95903329+break007@users.noreply.github.com>
2022-04-03 00:35:23 +0800

    Update Notify.cs

--- Notify.cs
+++ Notify.cs
@@ -8,7 +8,7 @@ public class Notify
     {
         var request = new RestRequest("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c4de16aa-91a8-496b-ae54-2c5d48bdc86d");
         request.AddHeader("Content-Type", "application/json");
-        request.AddJsonBody(new { msgtype = "text", text = new { content = content } });
+        request.AddJsonBody(new { msgtype = "text", text = new { content = title+"："+content } });
         var response = client.ExecutePostAsync(request);
         Log.Information(response.Result.Content);
 
